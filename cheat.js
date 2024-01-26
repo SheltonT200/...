be1f1f5b35aa870fd93047be55be901347dfdbc6
SheltonT200 <155662867+SheltonT200@users.noreply.github.com>
2024-01-26 09:38:17 -0600

    Add files via upload

---
+++ cheat.js
@@ -0,0 +1,15 @@
+function cheat(u){
+if (u==null||u==undefined){
+u = 10
+}
+for (let i = 0; i < 10; i++) {
+Luts.Upgrades.currency[i].d=[1,234,567,890]
+Luts.Upgrades.currency[i].e+=20
+}
+for (let o = 156; o < 174; o++){
+Luts.Upgrades.upgrades[o].level=Luts.Upgrades.upgrades[o].maxLevel-1//[156-173]
+}
+for (let x = 0; x < u; ++){
+Luts['Shop'].fakeList[4].androidCallback();
+}
+}
